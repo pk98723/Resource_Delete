23e7b026222089a48f4b417d12fe09d0000ee423
pk98723 <45585682+pk98723@users.noreply.github.com>
2025-07-29 18:32:40 +0530

    Update Storage_build.sh

--- Storage_build.sh
+++ Storage_build.sh
@@ -11,3 +11,6 @@ az storage container create --name lab2container --account-name $STORAGE_ACCOUNT
 
 # Upload a file
 az storage blob upload --account-name $STORAGE_ACCOUNT_NAME --container-name lab2container --name sample.txt --file ./sample.txt --auth-mode login
+
+az role assignment create --assignee $(az ad signed-in-user show --query id -o tsv) --role "Storage Blob Data Contributor" --scope "/subscriptions/e8b9650d-f8de-46cd-bec4-f688fae3b1c3/resourceGroups/myrg/providers/Microsoft.Storage/storageAccounts/mystr98723"
+
