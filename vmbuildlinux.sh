65a625ad66f0fbdc038cda905a4fea543d859aee
pk98723 <45585682+pk98723@users.noreply.github.com>
2025-08-15 22:35:50 +0530

    Update vmbuildlinux.sh

--- vmbuildlinux.sh
+++ vmbuildlinux.sh
@@ -1,71 +1,45 @@
 #!/bin/bash
+
+RESOURCE_GROUP="myrg"
+LOCATION="centralindia"
+VNET="myvnet"
+SUBNET1="mysubnet1"
+SUBNET2="mysubnet2"
+Vnet_PREFIX="10.0.0.0/16"
+PREFIX1="10.0.1.0/24"
+PREFIX2="10.0.2.0/24"
+NSG="mynsg"
+PUBLICIP="mypublicip"
+VMNAME="myvm"
+VMNIC="mynic"
+
 echo "Creating resource group..."
-az group create 
---name myrg 
---location centralindia
+az group create --name "$RESOURCE_GROUP" --location "$LOCATION"
 
 echo "Creating vnet..."
-az network vnet create 
---resource-group myrg 
---name myvnet 
---address-prefix 10.0.0.0/16
+az network vnet create --resource-group "$RESOURCE_GROUP" --name "$VNET" --address-prefix "$Vnet_PREFIX"
 
 echo "Creating first subnet..."
-az network vnet subnet create 
---resource-group myrg 
---vnet-name myvnet 
---name mysubnet 
---address-prefix 10.0.1.0/24
+az network vnet subnet create --resource-group "$RESOURCE_GROUP" --vnet-name "$VNET" --name "$SUBNET1" --address-prefix "$PREFIX1"
 
 echo "Creating second subnet..."
-az network vnet subnet create 
---resource-group myrg 
---vnet-name myvnet 
---name mysubnet1 
---address-prefix 10.0.2.0/24
+az network vnet subnet create --resource-group "$RESOURCE_GROUP" --vnet-name "$VNET" --name "$SUBNET2" --address-prefix "$PREFIX2"
 
 echo "Creating network security group..."
-az network nsg create 
---resource-group myrg 
---name mynsg
+az network nsg create --resource-group "$RESOURCE_GROUP" --name "$NSG"
 
 echo "Creating nsg rule..."
-az network nsg rule create 
---resource-group myrg 
---nsg-name mynsg 
---name Allow-rdp 
---priority 1001 
---access Allow  
---protocol TCP 
---direction Inbound 
---source-address-prefixes "*" 
---source-port-ranges "*" 
---destination-port-ranges 3389 
---destination-address-prefixes "*"
+az network nsg rule create --resource-group "$RESOURCE_GROUP" --nsg-name "$NSG" --name Allow-rdp --priority 1001 --access Allow --protocol TCP --direction Inbound --source-address-prefixes "*" 
+--source-port-ranges "*" --destination-port-ranges 3389 --destination-address-prefixes "*"
 
 echo "Creating public ip..."
-az network public-ip create 
---resource-group myrg 
---name mypubip
+az network public-ip create --resource-group "$RESOURCE_GROUP" --name "$PUBLICIP"
 
 echo "Creating nic and attaching to one subnet..."
-az network nic create 
---resource-group myrg 
---name mynic 
---vnet-name myvnet 
---subnet mysubnet 
---network-security-group mynsg 
---public-ip-address mypubip
+az network nic create --resource-group "$RESOURCE_GROUP" --name mynic --vnet-name "$VNET" --subnet "$SUBNET1" --network-security-group "$NSG" --public-ip-address "$PUBLICIP"
 
 echo "Creating VM and attaching nic..."
-az vm create 
---resource-group myrg  
---name myvm  
---nics mynic 
---image UbuntuLTS
---size Standard_B2ms 
---admin-username azureuser 
---admin-password 'P@ssw0rd@2025'
+az vm create --resource-group "$RESOURCE_GROUP" --name "$VMNAME" --nics "$VMNIC" --image UbuntuLTS --size Standard_B2ms --admin-username azureuser --admin-password 'P@ssw0rd@2025'
 
 echo "List the public created to the VM to login..."
-az vm list-ip-addresses -g myrg -n myvm
+az vm list-ip-addresses -g "$RESOURCE_GROUP" -n "$VMNAME"
